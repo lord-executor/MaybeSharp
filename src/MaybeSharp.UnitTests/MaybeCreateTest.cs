fa690e842785e75ac2a397db8c6f555134daa751
lord-executor <executor.dev@gmail.com>
2018-12-04 20:52:12 +0100

    Extension methods to create Maybe instances from other types

---
+++ src/MaybeSharp.UnitTests/MaybeCreateTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MaybeSharp.UnitTests
+{
+	[TestFixture]
+	public class MaybeCreateTest
+	{
+		[Test]
+		public void MaybeOf__Value__CreatesJustValue()
+		{
+			var obj = new DemoType();
+
+			var result = Maybe.Of(obj);
+
+			result.Should().NotBeNull();
+			result.Extract().Should().BeSameAs(obj);
+		}
+
+		[Test]
+		public void ToMaybe__FromValue__CreatesJustValue()
+		{
+			var obj = new DemoType();
+
+			var result = obj.ToMaybe();
+
+			result.Should().NotBeNull();
+			result.Extract().Should().BeSameAs(obj);
+		}
+
+		[Test]
+		public void MaybeOf__Null__CreatesNothing()
+		{
+			var result = Maybe.Of<DemoType>(null);
+
+			result.Should().NotBeNull();
+			result.Should().Be(Maybe.Nothing<DemoType>());
+		}
+
+		[Test]
+		public void ToMaybe__FromNull__CreatesNothing()
+		{
+			DemoType obj = null;
+
+			var result = obj.ToMaybe();
+
+			result.Should().NotBeNull();
+			result.Should().Be(Maybe.Nothing<DemoType>());
+		}
+	}
+}
---
+++ src/MaybeSharp.UnitTests/MaybeEnumerableTest.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MaybeSharp.UnitTests
+{
+	[TestFixture]
+	public class MaybeEnumerableTest
+	{
+		private List<DemoType> _emptyList = new List<DemoType>();
+		private List<DemoType> _singleList = new List<DemoType> { new DemoType("Single") };
+		private List<DemoType> _multiList = new List<DemoType> { new DemoType("First"), new DemoType("Second"), new DemoType("Last") };
+
+		[Test]
+		public void MaybeFirst__OfEmptyList__ReturnsNothing()
+		{
+			var result = _emptyList.MaybeFirst();
+
+			result.Should().NotBeNull();
+			result.Should().Be(Maybe.Nothing<DemoType>());
+		}
+
+		[Test]
+		public void MaybeFirst__OfList__ReturnsJustFirst()
+		{
+			var result = _multiList.MaybeFirst();
+
+			result.Should().NotBeNull();
+			result.Bind(d => d.Name).Extract().Should().Be("First");
+		}
+
+		[Test]
+		public void MaybeLast__OfEmptyList__ReturnsNothing()
+		{
+			var result = _emptyList.MaybeLast();
+
+			result.Should().NotBeNull();
+			result.Equals(Maybe.Nothing<DemoType>()).Should().BeTrue();
+		}
+
+		[Test]
+		public void MaybeLast__OfList__ReturnsJustFirst()
+		{
+			var result = _multiList.MaybeLast();
+
+			result.Should().NotBeNull();
+			result.Bind(d => d.Name).Extract().Should().Be("Last");
+		}
+
+		[Test]
+		public void MaybeSingle__OfEmptyList__ReturnsNothing()
+		{
+			var result = _emptyList.MaybeSingle();
+
+			result.Should().NotBeNull();
+			result.Equals(Maybe.Nothing<DemoType>()).Should().BeTrue();
+		}
+
+		[Test]
+		public void MaybeSingle__OfSingleItem__ReturnsJustItem()
+		{
+			var result = _singleList.MaybeSingle();
+
+			result.Should().NotBeNull();
+			result.Bind(d => d.Name).Extract().Should().Be("Single");
+		}
+	}
+}
